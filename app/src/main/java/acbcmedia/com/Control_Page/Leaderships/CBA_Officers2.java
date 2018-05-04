9b6dcc4ba0050fab2b0349f7b8ad1cdbacc717a8
vanimal37 <vmang37@gmail.com>
2018-05-03 21:58:04 -0400

    updated manifest

---
+++ app/src/main/java/acbcmedia/com/Control_Page/Leaderships/CBA_Officers2.java
@@ -0,0 +1,17 @@
+package acbcmedia.com.Control_Page.Leaderships;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import acbcmedia.com.Control_Page.R;
+
+
+public class CBA_Officers2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cba_officers2);
+
+    }
+}
